4e562caefd95d1c3f0551700441d696d1215025e
Rezvanankbaik <149947444+Rezvanankbaik@users.noreply.github.com>
2024-09-30 20:01:16 +0700

    test

--- javascript.js
+++ javascript.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["Designer","Bisnismen","Developer","Rezvan.H.A"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
